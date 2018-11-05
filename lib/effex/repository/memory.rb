7f089f6e6faffc36e23174428b1b7bdd8e7e35f2
Chris Tierney <notproperlycut@gmail.com>
2018-11-05 20:04:55 +0000

    Guess some repository and source methods
    This reverts commit c8759095615f3b356b7747bd2bc4ea21720c91d5.

--- lib/effex/repository/memory.rb
+++ lib/effex/repository/memory.rb
@@ -1,6 +1,19 @@
 module Effex
 	module Repository
 		class Memory < Base
+      def initialize
+        @rates = {}
+      end
+
+      def save(rate)
+        @rates[rate.date] ||= []
+        @rates[rate.date] << rate
+        return rate
+      end
+
+      def find_all_by_date(date)
+        @rates[date]
+      end
     end
   end
 end
--- lib/effex/source/base.rb
+++ lib/effex/source/base.rb
@@ -3,6 +3,13 @@ require 'effex/repository'
 module Effex
   module Source
     class Base
+      def retrieve
+        raise NotImplementedError, 'Source must implement the retrieve method'
+      end
+
+      def retrieve_and_save
+        retrieve.each { |r| Effex::Repository.for(:reference_rate).save(r) }
+      end
     end
   end
 end
